053f26d192c4b4846bb28e13e6b70e9729c9c27b
Adrian Armesto <armestomena@gmail.com>
2022-07-12 19:46:22 +0200

    Added NaN option

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const strictEquals = function (a, b) {
+  console.log("hi");
+
   if (Number.isNaN(a) && Number.isNaN(b)) {
+    return false;
+  } else if (Object.is(a, b)) {
+    return true;
   }
-  Object.is(a, b);
+  return "false";
 };
+console.log(strictEquals(1, 1));
+console.log(strictEquals(NaN, NaN));
+console.log(strictEquals(0, -0));
+console.log(strictEquals(-0, 0));
+console.log(strictEquals(1, "1"));
+console.log(strictEquals(true, false));
+console.log(strictEquals(false, false));
+console.log(strictEquals("Water", "Oli"));
